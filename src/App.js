6133e9b5d8ac6936d1fc204318752e7d91ff0dc2
Ayush <ayushrawat11562@gmail.com>
2021-04-09 22:07:28 +0530

    css half-done

--- src/App.js
+++ src/App.js
@@ -4,15 +4,12 @@ import {
   Switch,
   Route
 } from 'react-router-dom';
-import Navbar from './components/Navbar';
-
 import Home from './components/pages/Home';
-
 
 function App() {
   return (
     <Router>
-      <Navbar />
+
       <Switch>
           <Route path='/' component={Home} />
       </Switch>
